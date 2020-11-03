e587fdbb009274157b3ac233eb0496b77e7f2be8
arilrve <dog77879@gmail.com>
2020-11-03 07:41:56 -0800

    Initial Commit

---
+++ Test_10-26/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_10-26
+//
+//  Created by Andy Arilrve on 2020/11/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
